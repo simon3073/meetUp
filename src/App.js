e14c3fe3e6bead651a7227f9ec7c5c0a412cf858
simon3073 <simonbertoli@gmail.com>
2022-07-14 20:16:07 +1000

    Exercise 4.8 Submission - App converted to PWA with verification

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React, { Component } from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import WelcomeScreen from './WelcomeScreen';
 import { Grid, Segment, Divider } from 'semantic-ui-react';
 import './App.css';
 import './nprogress.css';
 
 class App extends Component {
 	state = {
+		showWelcomeScreen: undefined,
 		events: [],
 		locations: [],
 		locationSet: 'All Cities',
 		eventNo: 32
 	};
 
-	componentDidMount() {
+	async componentDidMount() {
 		this.mounted = true;
-
-		getEvents().then((events) => {
-			if (this.mounted) {
-				this.setState({ events, locations: extractLocations(events) });
-				this.updateEvents();
-			}
-		});
+		const accessToken = localStorage.getItem('access_token');
+		const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+		const searchParams = new URLSearchParams(window.location.search);
+		const code = searchParams.get('code');
+		this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+		if ((code || isTokenValid) && this.mounted) {
+			getEvents().then((events) => {
+				if (this.mounted) {
+					this.setState({ events, locations: extractLocations(events) });
+					this.updateEvents();
+				}
+			});
+		}
 	}
 
 	componentWillUnmount() {
@@ -40,25 +48,33 @@ class App extends Component {
 	};
 
 	render() {
-		const { locationSet } = this.state;
+		const { events, locations, locationSet, showWelcomeScreen } = this.state;
+		if (showWelcomeScreen === undefined) return <div className="App" />;
+
 		return (
 			<div className="App">
-				<Grid className="grid-header" stackable columns={2} divided>
-					<div className="header-logo"></div>
+				{showWelcomeScreen ? (
+					<WelcomeScreen showWelcomeScreen={showWelcomeScreen} getAccessToken={() => getAccessToken()} />
+				) : (
+					<>
+						<Grid className="grid-header" stackable columns={2} divided>
+							<div className="header-logo"></div>
 
-					<Grid.Row className="grid-row-header">
-						<Grid.Column>
-							<Segment className="header-segment">
-								<CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-								<NumberOfEvents updateEvents={this.updateEvents} />
-							</Segment>
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-				<Divider horizontal>
-					<span className="event-header">{locationSet === 'All Cities' ? 'All Events' : `Events in ${locationSet}`}</span>
-				</Divider>
-				<EventList events={this.state.events} />
+							<Grid.Row className="grid-row-header">
+								<Grid.Column>
+									<Segment className="header-segment">
+										<CitySearch locations={locations} updateEvents={this.updateEvents} />
+										<NumberOfEvents updateEvents={this.updateEvents} />
+									</Segment>
+								</Grid.Column>
+							</Grid.Row>
+						</Grid>
+						<Divider horizontal>
+							<span className="event-header">{locationSet === 'All Cities' ? 'All Events' : `Events in ${locationSet}`}</span>
+						</Divider>
+						<EventList events={events} />
+					</>
+				)}
 			</div>
 		);
 	}
