168cccb7dd0edde547e81890cea1f8c3a6d54f6c
simon3073 <simonbertoli@gmail.com>
2022-07-21 20:35:41 +1000

    Move Event Details out of event to work with Grid, update event styling, add windowResize Hook

--- src/Event.js
+++ src/Event.js
@@ -1,78 +1,50 @@
-import React, { Component } from 'react'
-import {
-  Button,
-  Label,
-  Icon,
-  Transition,
-  Grid,
-  Segment,
-} from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class Event extends Component {
-  state = {
-    showDetails: false,
-  }
+    state = {
+        showDetails: false,
+    };
 
-  handleButtonClick = () => {
-    this.setState({ showDetails: !this.state.showDetails })
-  }
+    handleButtonClick = async () => {
+        this.setState({ showDetails: !this.state.showDetails });
+        this.props.showEventDetails(this.state.showDetails ? null : this.props.eventId);
+    };
 
-  displayDate = (d) => {
-    const date = new Date(d)
-    return (
-      date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
-    )
-  }
+    displayDate = (d) => {
+        const date = new Date(d);
+        return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    };
 
-  render() {
-    const { event, evenOrOdd } = this.props
-    const eventWidth = this.state.showDetails ? 16 : ''
-    return (
-      <Grid.Column width={eventWidth} key={event.id}>
-        <Segment className="event-block">
-          <div className="event">
-            <span className="summary">
-              <h2>{event.summary}</h2>
-            </span>
-            <p className="date">{this.displayDate(event.start.dateTime)}</p>
-            <p className="location">{event.location}</p>
-            <Button
-              inverted
-              color="yellow"
-              className="details-btn"
-              size="large"
-              onClick={this.handleButtonClick}
-            >
-              {this.state.showDetails ? 'Hide Details' : 'Show Details'}
-            </Button>
+    componentDidUpdate() {
+        if (!this.props.eventDetailsBool && this.state.showDetails) {
+            this.setState({ showDetails: false });
+        }
+    }
 
-            {/* {this.state.showDetails ? ( */}
-            <Transition
-              visible={this.state.showDetails}
-              animation="fade down"
-              duration={{ hide: 0, show: 500 }}
-            >
-              <div className="event-details">
-                <a className="event-link" href="{event.htmlLink}">
-                  Google Calendar Event
-                </a>
-                <p className="event-desc">{event.description}</p>
-                <Label color="teal" className="event-email" size="large">
-                  <Icon name="mail" />
-                  <a href="mailto:{event.organizer.email}">
-                    Email for more info
-                  </a>
-                </Label>
-              </div>
-            </Transition>
-            {/* ) : (
-            ''
-          )} */}
-          </div>
-        </Segment>
-      </Grid.Column>
-    )
-  }
+    render() {
+        const { event } = this.props;
+        return (
+            <>
+                <div className="event">
+                    <span className="summary">
+                        <h2>{event.summary}</h2>
+                    </span>
+                    <p className="date">{this.displayDate(event.start.dateTime)}</p>
+                    <p className="location">{event.location}</p>
+                    <Button
+                        inverted
+                        color="yellow"
+                        className="details-btn"
+                        size="large"
+                        onClick={this.handleButtonClick}
+                    >
+                        {this.state.showDetails ? 'Hide Details' : 'Show Details'}
+                    </Button>
+                </div>
+            </>
+        );
+    }
 }
 
-export default Event
+export default Event;
--- src/EventList.js
+++ src/EventList.js
@@ -1,36 +1,75 @@
-import React from 'react'
-import Event from './Event'
-import { Grid, Segment } from 'semantic-ui-react'
+import { React, useState } from 'react';
+import Event from './Event';
+import { Grid, Segment, Label, Icon } from 'semantic-ui-react';
+import useWindowSize from './useWindowSize';
 
 export default function EventList(props) {
-  const { events } = props
+    const { events } = props;
+    const [detailsIndex, setdetailsIndex] = useState(null);
+    const oneColumn = useWindowSize().width < 768;
+    let addToNextRow = 'null';
 
-  const changeWidth = (val) => {
-    console.log('🚀 ~ file: EventList.js ~ line 9 ~ EventList ~ val', val)
-  }
+    const showDetails = (eventToShow, arrowPos = 'arrow-el') => {
+        return (
+            <Grid.Column width={16} key={eventToShow.id} className="event-details-block">
+                <div className={arrowPos}>
+                    <div className="arrow"></div>
+                </div>
+                <Segment className="segment-block-details">
+                    <div className="event-details">
+                        <a className="event-link" href="{eventToShow.htmlLink}">
+                            Google Calendar Event
+                        </a>
+                        <p className="event-desc">{eventToShow.description}</p>
+                        <Label color="red" className="event-email" size="big">
+                            <Icon name="mail" />
+                            <a href="mailto:{eventToShow.organizer.email}">Email for more info</a>
+                        </Label>
+                    </div>
+                </Segment>
+            </Grid.Column>
+        );
+    };
 
-  return (
-    <>
-      <Grid stackable columns={2} className="event-grid" padded="horizontally">
-        {events.map((event, i) => (
-          <Event
-            key={event.id}
-            event={event}
-            changeWidth={changeWidth}
-            evenOrOdd={i % 2 === 0}
-          />
-        ))}
-      </Grid>
-    </>
-  )
+    // function to calculate where to show the details
+    const calculateDetailVisibility = (i) => {
+        const eventDetails = events[detailsIndex]; // set event data to the state saved
+        if (detailsIndex === i && oneColumn) {
+            return showDetails(eventDetails); // if we are seeing only one column then show directly underneath
+        }
+
+        // otherwise calculate if this is the first event displayed (of two)
+        // if it is delay its addition to the grid until the next loop
+        const isLastInRow = i % 2 !== 0;
+        if ((detailsIndex === i && isLastInRow) || i === addToNextRow) {
+            return showDetails(eventDetails, addToNextRow !== 'null' ? 'arrow-el left' : 'arrow-el right');
+        } else if (detailsIndex === i) {
+            addToNextRow = i + 1;
+        }
+    };
+
+    return (
+        <Grid stackable columns={2} className="event-grid aaaa" padded="horizontally">
+            {events.map((event, i) => (
+                <>
+                    {/* {test()} */}
+                    <Grid.Column key={event.id}>
+                        <Segment className="event-block">
+                            <Event
+                                key={event.id}
+                                event={event}
+                                eventDetailsBool={detailsIndex === i}
+                                eventId={i}
+                                showEventDetails={(e) => setdetailsIndex(e)}
+                            />
+                        </Segment>
+                    </Grid.Column>
+                    {
+                        // show details of an event based on the state > which is passed up from the event when button is clicked
+                        detailsIndex !== null ? calculateDetailVisibility(i) : ''
+                    }
+                </>
+            ))}
+        </Grid>
+    );
 }
-
-//  ;<Grid stackable columns={2} className="event-grid" padded="horizontally">
-//    {events.map((event) => (
-//      <Grid.Column width={12} key={event.id}>
-//        <Segment className="event-block">
-//          <Event event={event} changeWidth={changeWidth} />
-//        </Segment>
-//      </Grid.Column>
-//    ))}
-//  </Grid>
